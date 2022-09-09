dec6ea0b24e590ea14447f21f65e872910c37cb5
Maicon Gerardi <maicon.gerardi@gmail.com>
2022-09-09 17:48:15 -0300

    teste

--- src/main/java/com/dbc/pessoaapi/config/OpenApiConfig.java
+++ src/main/java/com/dbc/pessoaapi/config/OpenApiConfig.java
@@ -8,14 +8,18 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+
 
 @Hidden
 @Configuration
@@ -28,12 +32,13 @@ public class OpenApiConfig {
     }
 
     @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI springShopOpenAPI(ServletContext servletContext) {
         String securitySchemeName = "bearerAuth";
 //        Server server = new Server();
 //        server.setUrl("http://vemser-dbc.dbccompany.com.br:39000/dbc-pessoa-api/");
+        Server server = new Server().url(servletContext.getContextPath());
         return new OpenAPI()
-//                .addServersItem(server)
+                .servers(List.of(server))
                 .paths(new Paths())
                 .info(new Info().title("Pessoa API")
                         .description("Pessoa API documentação")
